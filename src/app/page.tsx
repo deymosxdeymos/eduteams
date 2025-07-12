ffc9ec7b108114052c833509c2ad7d9902ccfaa7
Galin Nichola <galinnichola15@gmail.com>
2025-07-12 19:24:48 +0700

    fix: reduce the width of the chat bubble stem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
             <div className='relative text-center text-3xl font-semibold w-56 rounded-full bg-white p-4 text-blue-800'>
               Masuk
               <div className='absolute z-10 right-0'>
-                <div className='rounded-sm before:rounded-full before:absolute before:-bottom-10.5 before:-left-17 before:h-14 before:w-8 before:rotate-42 before:transform before:border-r-2 before:border-t-2 before:border-white before:bg-white'></div>
+                <div className='rounded-sm before:rounded-full before:absolute before:-bottom-9.5 before:-left-17 before:h-14 before:w-7 before:rotate-42 before:transform before:border-r-2 before:border-t-2 before:border-white before:bg-white'></div>
               </div>
             </div>
           </div>
