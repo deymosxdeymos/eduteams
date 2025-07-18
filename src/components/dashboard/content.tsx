601ed8c4ece45972d2fb74befde38b6ee552ed18
Galin Nichola <galinnichola15@gmail.com>
2025-07-18 17:12:55 +0700

    feat: sliced the anda membuat kelas and buat kelas baru on content dashboard

--- src/components/dashboard/content.tsx
+++ src/components/dashboard/content.tsx
@@ -1,4 +1,6 @@
-import { Search } from 'lucide-react';
+import Image from 'next/image';
+import { Search, Plus } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 export default function Content() {
   return (
     <div className='h-full flex flex-col gap-4'>
@@ -35,6 +37,28 @@ export default function Content() {
             />
             <Search className='w-5 h-5 text-gray-400' />
           </div>
+        </div>
+        <div className='flex flex-col items-center justify-center gap-y-4 mx-auto h-full'>
+          <Image
+            src='/belum-kelas.svg'
+            width={180}
+            height={180}
+            alt='belum kelas'
+          />
+          <div className='text-center'>
+            <h1 className='text-3xl font-semibold text-gray-800 tracking-tight pb-2'>
+              Anda belum membuat kelas
+            </h1>
+            <p className='text-gray-600 text-sm font-normal'>
+              Buat kelas untuk memulai pembagian kelompok
+            </p>
+          </div>
+          <Button variant='onboarding' className='rounded-full w-48 py-7'>
+            <Plus strokeWidth={3} className=' text-white' />
+            <p className='text-white font-semibold text-base leading-tight'>
+              Buat Kelas Baru
+            </p>
+          </Button>
         </div>
       </div>
     </div>
