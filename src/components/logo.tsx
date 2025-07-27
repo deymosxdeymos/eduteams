ec042f5c12ecc0f503533e625db378d39e24ce32
deymos <galinnichola15@gmail.com>
2025-07-27 17:59:59 +0700

    chore: update lucide-react version and clean up SVG files
    - Upgrade lucide-react dependency from version 0.525.0 to 0.526.0 in bun.lock and package.json
    - Remove unused laki.svg file
    - Modify mahasiswa.svg file to change its mode and update its content
    - Refactor page.tsx to enhance layout and add contact information with icons
    - Update logo component to support additional props for customization

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,22 +3,30 @@ import Image from 'next/image';
 
 interface LogoProps {
   color?: 'white' | 'black';
+  className?: string;
+  size?: string;
 }
 
-export default function Logo({ color = 'white' }: LogoProps) {
+export default function Logo({
+  color = 'white',
+  className,
+  size = 'text-3xl',
+}: LogoProps) {
   const mainTextClass = color === 'black' ? 'text-black' : 'text-white';
   const spanTextClass = color === 'black' ? 'text-neutral-800' : 'text-white';
 
   return (
-    <header className='flex flex-row justify-center items-center pt-14 w-full gap-2'>
+    <header
+      className={`flex flex-row items-center w-full gap-8 ${className || ''}`}
+    >
       <Image
-        src='/logo.png'
+        src='/mascot-yellow-head.svg'
         width={50}
         height={50}
         alt='logo'
         className='w-auto h-auto'
       />
-      <p className={`${mainTextClass} text-3xl font-bold`}>
+      <p className={`${mainTextClass} ${size} font-bold`}>
         Equi<span className={`${spanTextClass} font-light`}>Team</span>
       </p>
     </header>
